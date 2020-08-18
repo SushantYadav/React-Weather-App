ce5d41ecc8a7b368405be76bffa64c677b5fd8d5
Sushant Yadav <sushanty@techepoch.com>
2020-08-18 15:56:27 +0530

    Updates

---
+++ precache-manifest.1825241dd2f6d2968916013f8eba24fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "028fbbaf5d597b76362c1e211f02352e",
+    "url": "/React-Weather-App/index.html"
+  },
+  {
+    "revision": "b91f3250b88b033cc8d1",
+    "url": "/React-Weather-App/static/css/main.15fb3d4c.chunk.css"
+  },
+  {
+    "revision": "3c24c48148cb81095b7e",
+    "url": "/React-Weather-App/static/js/2.21de60f6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Weather-App/static/js/2.21de60f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b91f3250b88b033cc8d1",
+    "url": "/React-Weather-App/static/js/main.1ddd35fe.chunk.js"
+  },
+  {
+    "revision": "eb98bfa99ad58471292a",
+    "url": "/React-Weather-App/static/js/runtime-main.79b0b56a.js"
+  }
+]);
